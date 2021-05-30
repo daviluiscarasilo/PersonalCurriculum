6120a05995fe0f9b886e77fce577f2b05c0ee554
daviluiscarasilo <carasilo.daviluis@gmail.com>
2021-05-30 12:46:01 -0300

    Change metadata

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 
 import { MdCollectionsBookmark } from 'react-icons/md'
+import profileImage from './../media/android-chrome-512x512.svg'
 
 const Rodape = () => {
     return (
 <Navbar bg="dark" fixed="bottom" >
     <Navbar.Brand href="https://www.linkedin.com/in/davi-luis-carasilo-934338171" target="_blank" className="text-light">
-        <MdCollectionsBookmark/> Developed by: Davi L. Carasilo - Back-End  software engineer
+        <MdCollectionsBookmark/> Developed by: Davi L. Carasilo
     </Navbar.Brand>
 </Navbar>
     )
